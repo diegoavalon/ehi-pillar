1071f7dbfb6a46e3cce2588168c8b22dcb1a2911
Diego Avalon <diego.avalon@ehealth.com>
2019-08-16 10:18:42 -0700

    adds w-auto class

--- ehi-pillar-config.js
+++ ehi-pillar-config.js
@@ -1,106 +1,107 @@
 module.exports = {
     theme: {
         colors: {
-            black: "var(--black)",
-            white: "var(--white)",
+            black: 'var(--black)',
+            white: 'var(--white)',
 
-            "gray-active": "var(--gray-active)",
-            "gray-hover": "var(--gray-hover)",
-            "gray-dark": "var(--gray-dark)",
-            gray: "var(--gray)",
-            "gray-lighter": "var(--gray-lighter)",
-            "gray-lightest": "var(--gray-lightest)",
+            'gray-active': 'var(--gray-active)',
+            'gray-hover': 'var(--gray-hover)',
+            'gray-dark': 'var(--gray-dark)',
+            gray: 'var(--gray)',
+            'gray-lighter': 'var(--gray-lighter)',
+            'gray-lightest': 'var(--gray-lightest)',
 
-            "red-active": "var(--red-active)",
-            "red-hover": "var(--red-hover)",
-            "red-text": "var(--red-text)",
-            "red-dark": "var(--red-dark)",
-            red: "var(--red)",
-            "red-light": "var(--red-light)",
-            "red-lightest": "var(--red-lightest)",
+            'red-active': 'var(--red-active)',
+            'red-hover': 'var(--red-hover)',
+            'red-text': 'var(--red-text)',
+            'red-dark': 'var(--red-dark)',
+            red: 'var(--red)',
+            'red-light': 'var(--red-light)',
+            'red-lightest': 'var(--red-lightest)',
 
-            "blue-active": "var(--blue-active)",
-            "blue-hover": "var(--blue-hover)",
-            "blue-text": "var(--blue-text)",
-            blue: "var(--blue)",
-            "blue-light": "var(--blue-light)",
-            "blue-lightest": "var(--blue-lightest)",
+            'blue-active': 'var(--blue-active)',
+            'blue-hover': 'var(--blue-hover)',
+            'blue-text': 'var(--blue-text)',
+            blue: 'var(--blue)',
+            'blue-light': 'var(--blue-light)',
+            'blue-lightest': 'var(--blue-lightest)',
 
-            "orange-active": "var(--orange-active)",
-            "orange-hover": "var(--orange-hover)",
-            "orange-text": "var(--orange-text)",
-            orange: "var(--orange)",
-            "orange-lightest": "var(--orange-lightest)",
+            'orange-active': 'var(--orange-active)',
+            'orange-hover': 'var(--orange-hover)',
+            'orange-text': 'var(--orange-text)',
+            orange: 'var(--orange)',
+            'orange-lightest': 'var(--orange-lightest)',
 
-            "green-active": "var(--green-active)",
-            "green-hover": "var(--green-hover)",
-            "green-text": "var(--green-text)",
-            green: "var(--green)",
-            "green-light": "var(--green-light)",
-            "green-lightest": "var(--green-lightest)"
+            'green-active': 'var(--green-active)',
+            'green-hover': 'var(--green-hover)',
+            'green-text': 'var(--green-text)',
+            green: 'var(--green)',
+            'green-light': 'var(--green-light)',
+            'green-lightest': 'var(--green-lightest)'
         },
         spacing: {
-            "0": "0",
-            xxxxs: "calc(0.125 * var(--space-unit))",
-            xxxs: "calc(0.25 * var(--space-unit))",
-            xxs: "calc(0.375 * var(--space-unit))",
-            xs: "calc(0.5 * var(--space-unit))",
-            sm: "calc(0.75 * var(--space-unit))",
-            base: "var(--space-unit)",
-            md: "calc(1.25 * var(--space-unit))",
-            lg: "calc(2 * var(--space-unit))",
-            xl: "calc(3.25 * var(--space-unit))",
-            xxl: "calc(5.25 * var(--space-unit))",
-            xxxl: "calc(8.5 * var(--space-unit))",
-            xxxxl: "calc(13.75 * var(--space-unit))",
-            component: "calc(1.25 * var(--space-unit))"
+            '0': '0',
+            xxxxs: 'calc(0.125 * var(--space-unit))',
+            xxxs: 'calc(0.25 * var(--space-unit))',
+            xxs: 'calc(0.375 * var(--space-unit))',
+            xs: 'calc(0.5 * var(--space-unit))',
+            sm: 'calc(0.75 * var(--space-unit))',
+            base: 'var(--space-unit)',
+            md: 'calc(1.25 * var(--space-unit))',
+            lg: 'calc(2 * var(--space-unit))',
+            xl: 'calc(3.25 * var(--space-unit))',
+            xxl: 'calc(5.25 * var(--space-unit))',
+            xxxl: 'calc(8.5 * var(--space-unit))',
+            xxxxl: 'calc(13.75 * var(--space-unit))',
+            component: 'calc(1.25 * var(--space-unit))'
         },
         width: {
-            "1-2": "50%",
-            "1-3": "33.33333%",
-            "2-3": "66.66667%",
-            "1-4": "25%",
-            "3-4": "75%",
-            "1-5": "20%",
-            "2-5": "40%",
-            "3-5": "60%",
-            "4-5": "80%",
-            "1-6": "16.66667%",
-            "5-6": "83.33333%",
-            full: "100%",
-            screen: "100vw"
+            '1-2': '50%',
+            '1-3': '33.33333%',
+            '2-3': '66.66667%',
+            '1-4': '25%',
+            '3-4': '75%',
+            '1-5': '20%',
+            '2-5': '40%',
+            '3-5': '60%',
+            '4-5': '80%',
+            '1-6': '16.66667%',
+            '5-6': '83.33333%',
+            full: '100%',
+            screen: '100vw',
+            auto: 'auto'
         },
         opacity: {
-            "0": "0",
-            "20": "0.2",
-            "40": "0.4",
-            "60": "0.6",
-            "80": "0.8",
-            "100": "1"
+            '0': '0',
+            '20': '0.2',
+            '40': '0.4',
+            '60': '0.6',
+            '80': '0.8',
+            '100': '1'
         },
         borderRadius: {
-            none: "0",
-            sm: "5px",
-            default: "10px",
-            lg: "15px",
-            full: "9999px"
+            none: '0',
+            sm: '5px',
+            default: '10px',
+            lg: '15px',
+            full: '9999px'
         },
         screens: {
-            phablet: "calc(576px + 40px)",
-            tablet: "calc(768px + 80px)",
-            laptop: "calc(992px + 120px)",
-            desktop: "calc(1200px + 200px)"
+            phablet: 'calc(576px + 40px)',
+            tablet: 'calc(768px + 80px)',
+            laptop: 'calc(992px + 120px)',
+            desktop: 'calc(1200px + 200px)'
         },
         fontWeight: {
-            regular: "400",
-            "semi-bold": "600",
-            bold: "700",
-            "extra-bold": "800",
-            black: "900"
+            regular: '400',
+            'semi-bold': '600',
+            bold: '700',
+            'extra-bold': '800',
+            black: '900'
         },
         fontFamily: {
-            sans: ["Muli", "sans-serif"],
-            serif: ["Libre Baskerville", "serif"]
+            sans: ['Muli', 'sans-serif'],
+            serif: ['Libre Baskerville', 'serif']
         },
         // boxShadow: {
         //     "1dp":
@@ -125,8 +126,8 @@ module.exports = {
         //         "0 24px 38px 3px rgba(47, 105, 252, .08), 0 9px 46px 8px rgba(47, 105, 252, .08), 0 11px 15px -7px rgba(47, 105, 252, .12)"
         // },
         margin: theme => ({
-            auto: "auto",
-            ...theme("spacing")
+            auto: 'auto',
+            ...theme('spacing')
         })
     },
     variants: {},
