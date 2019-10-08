b156cd36c2542436ad8c62f542a87283be89cb77
Diego Avalon <diego.avalon@ehealth.com>
2019-10-08 13:25:32 -0700

    adds font inheritance to form elements, css variable transpilation, and CSS grid support for autoprefixing

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,9 @@ import browsersync from 'rollup-plugin-browsersync';
 import commonjs from 'rollup-plugin-commonjs';
 import css from 'rollup-plugin-css-only';
 import cssnano from 'cssnano';
-import { eslint } from 'rollup-plugin-eslint';
+import {
+	eslint
+} from 'rollup-plugin-eslint';
 import fs from 'fs';
 import license from 'rollup-plugin-license';
 import postcss from 'postcss';
@@ -12,144 +14,157 @@ import filesize from 'rollup-plugin-filesize';
 import resolve from 'rollup-plugin-node-resolve';
 import sass from 'rollup-plugin-sass';
 import stylelint from 'rollup-plugin-stylelint';
-import { terser } from 'rollup-plugin-terser';
+import {
+	terser
+} from 'rollup-plugin-terser';
 import visualizer from 'rollup-plugin-visualizer';
+import cssvariables from 'postcss-css-variables';
 
 const pkg = require('./package.json');
 const banner = ['/*!', pkg.name, pkg.version, '*/\n'].join(' ');
 
 const sassOptions = {
-    output(styles, styleNodes) {
-        fs.mkdirSync('dist/css', { recursive: true }, err => {
-            if (err) {
-                throw err;
-            }
-        });
+	output(styles, styleNodes) {
+		fs.mkdirSync('dist/css', {
+			recursive: true
+		}, err => {
+			if (err) {
+				throw err;
+			}
+		});
 
-        styleNodes.forEach(({ id, content }) => {
-            const scssName = id.substring(id.lastIndexOf('/') + 1, id.length);
-            const name = scssName.split('.')[0];
-            fs.writeFileSync(`dist/css/${name}.css`, content);
-        });
-    },
-    processor: css =>
-        postcss([
-            autoprefixer({
-                grid: false
-            }),
-            cssnano()
-        ])
-            .process(css)
-            .then(result => result.css)
+		styleNodes.forEach(({
+			id,
+			content
+		}) => {
+			const scssName = id.substring(id.lastIndexOf('/') + 1, id.length);
+			const name = scssName.split('.')[0];
+			fs.writeFileSync(`dist/css/${name}.css`, content);
+		});
+	},
+	processor: css =>
+		postcss([
+			cssvariables(),
+			autoprefixer({
+				grid: true
+			}),
+			cssnano()
+		])
+		.process(css)
+		.then(result => result.css)
 };
 
 const plugins = [
-    resolve(),
-    commonjs(),
-    stylelint({
-        fix: false,
-        include: ['src/**.scss'],
-        syntax: 'scss',
-        quiet: false
-    }),
-    eslint(),
-    babel({
-        exclude: 'node_modules/**'
-    }),
-    css({ output: false })
+	resolve(),
+	commonjs(),
+	stylelint({
+		fix: false,
+		include: ['src/**.scss'],
+		syntax: 'scss',
+		quiet: false
+	}),
+	eslint(),
+	babel({
+		exclude: 'node_modules/**'
+	}),
+	css({
+		output: false
+	})
 ];
 
 if (!process.env.DEVELOPMENT) {
-    plugins.push(
-        sass({
-            output: false
-        })
-    );
+	plugins.push(
+		sass({
+			output: false
+		})
+	);
 }
 
 // If we are running with --environment DEVELOPMENT, serve via browsersync for local development
 if (process.env.DEVELOPMENT) {
-    plugins.push(sass(sassOptions));
+	plugins.push(sass(sassOptions));
 
-    plugins.push(
-        browsersync({
-            host: 'localhost',
-            watch: true,
-            port: 3000,
-            notify: false,
-            open: true,
-            server: {
-                baseDir: 'playground',
-                routes: {
-                    '/ehi-pillar/dist/css/main.css': 'dist/css/main.css',
-                    '/ehi-pillar/dist/js/main.js': 'dist/js/main.js'
-                }
-            }
-        })
-    );
+	plugins.push(
+		browsersync({
+			host: 'localhost',
+			watch: true,
+			port: 3000,
+			notify: false,
+			open: true,
+			server: {
+				baseDir: 'playground',
+				routes: {
+					'/ehi-pillar/dist/css/main.css': 'dist/css/main.css',
+					'/ehi-pillar/dist/js/main.js': 'dist/js/main.js'
+				}
+			}
+		})
+	);
 }
 
-plugins.push(license({ banner }));
+plugins.push(license({
+	banner
+}));
 plugins.push(filesize());
 plugins.push(visualizer());
 
 const rollupBuilds = [
-    // Generate unminifed bundle
-    {
-        input: './src/js/main.js',
+	// Generate unminifed bundle
+	{
+		input: './src/js/main.js',
 
-        output: [
-            {
-                file: pkg.main,
-                format: 'umd',
-                name: 'Main',
-                sourcemap: true
-            },
-            {
-                file: pkg.module,
-                format: 'esm',
-                sourcemap: true
-            }
-        ],
-        plugins
-    }
+		output: [{
+				file: pkg.main,
+				format: 'umd',
+				name: 'Main',
+				sourcemap: true
+			},
+			{
+				file: pkg.module,
+				format: 'esm',
+				sourcemap: true
+			}
+		],
+		plugins
+	}
 ];
 
 if (!process.env.DEVELOPMENT) {
-    rollupBuilds.push(
-        // Generate minifed bundle
-        {
-            input: './src/js/main.js',
-            output: [
-                {
-                    file: 'dist/js/main.min.js',
-                    format: 'umd',
-                    name: 'Main',
-                    sourcemap: true
-                },
-                {
-                    file: 'dist/js/main.esm.min.js',
-                    format: 'esm',
-                    sourcemap: true
-                }
-            ],
-            plugins: [
-                resolve(),
-                commonjs(),
-                babel({
-                    exclude: 'node_modules/**'
-                }),
-                sass(sassOptions),
-                css({ output: false }),
-                terser(),
-                license({
-                    banner
-                }),
-                filesize(),
-                visualizer()
-            ]
-        }
-    );
+	rollupBuilds.push(
+		// Generate minifed bundle
+		{
+			input: './src/js/main.js',
+			output: [{
+					file: 'dist/js/main.min.js',
+					format: 'umd',
+					name: 'Main',
+					sourcemap: true
+				},
+				{
+					file: 'dist/js/main.esm.min.js',
+					format: 'esm',
+					sourcemap: true
+				}
+			],
+			plugins: [
+				resolve(),
+				commonjs(),
+				babel({
+					exclude: 'node_modules/**'
+				}),
+				sass(sassOptions),
+				css({
+					output: false
+				}),
+				terser(),
+				license({
+					banner
+				}),
+				filesize(),
+				visualizer()
+			]
+		}
+	);
 }
 
 export default rollupBuilds;
